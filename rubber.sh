7f01c1c24ae3d3e9f5c9ebc995d009cec556e1bb
Matt Conway <wr0ngway@yahoo.com>
2007-03-28 20:29:36 +0000

    initial import
    git-svn-id: svn+ssh://rubyforge.org/var/svn/rubber/trunk@1 04602977-1bcd-48be-894b-5e2845e2d00e

---
+++ rubber.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+ruby src/rubber.rb
---
+++ src/rubber.rb
@@ -0,0 +1,11 @@
+#
+# Rubber, James Rubber
+#
+module Rubber
+	VERSION = '0.0.0.0'
+end
+
+if __FILE__ == $0
+	puts "Rubber, version #{Rubber::VERSION}"
+	puts 'Hello World! :)'
+end
