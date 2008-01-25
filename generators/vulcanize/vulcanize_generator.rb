cab517a143e76da2cd48ef4d56344e15d68cadce
Matt Conway <wr0ngway@yahoo.com>
2008-01-25 21:19:25 +0000

    move capfile out, better svn/cvs check
    git-svn-id: svn+ssh://rubyforge.org/var/svn/rubber/trunk@35 04602977-1bcd-48be-894b-5e2845e2d00e

--- generators/vulcanize/vulcanize_generator.rb
+++ generators/vulcanize/vulcanize_generator.rb
@@ -6,28 +6,26 @@ class VulcanizeGenerator < Rails::Generator::NamedBase
     record do |m|
       sp = source_path("#{file_name}/")
       unless File.directory?(sp)
-        templates = Dir.entries(source_root).delete_if {|e| e =~ /(^\.)|svn|CVS/ }
-        raise Rails::Generator::UsageError.new("Invalid template #{file_name}, use one of #{valid_templates}")
+        raise Rails::Generator::UsageError.new("Invalid template #{file_name}, use one of #{valid_templates.join(', ')}")
       end
 
-      m.file("#{file_name}/Capfile", 'Capfile')
       Find.find(sp) do |f|
-        Find.prune if f =~ /CVS|svn|Capfile/
+        Find.prune if f =~ /^(CVS|\.svn)$/
         rel = f.gsub(/#{source_root}\//, '')
         dest_rel = "config/" + rel.gsub(/#{file_name}\//, '')
         m.directory(dest_rel) if File.directory?(f)
         m.file(rel, dest_rel) if File.file?(f)
       end
+      m.file("Capfile", "Capfile")
     end
   end
 
   protected
     def valid_templates
       valid = Dir.entries(File.dirname(__FILE__) + "/templates").delete_if {|e| e =~ /(^\.)|svn|CVS/ }
-      valid.join(", ")
     end
 
     def banner
-      "Usage: #{$0} vulcanize template_name\n\twhere template_name is one of #{valid_templates}"
+      "Usage: #{$0} vulcanize template_name\n\twhere template_name is one of #{valid_templates.join(', ')}"
     end
 end
