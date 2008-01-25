eacb48552da70a43cca9e590549c32b9fb16ccb2
Matt Conway <wr0ngway@yahoo.com>
2008-01-25 23:26:22 +0000

    fix db primary role check
    git-svn-id: svn+ssh://rubyforge.org/var/svn/rubber/trunk@45 04602977-1bcd-48be-894b-5e2845e2d00e

--- test/configuration_test.rb
+++ test/configuration_test.rb
@@ -246,12 +246,12 @@ end
     instance.add(i1 = InstanceItem.new('host1', [RoleItem.new('role1')], ''))
     instance.add(i2 = InstanceItem.new('host2', [RoleItem.new('role1')], ''))
     instance.add(i3 = InstanceItem.new('host3', [RoleItem.new('role2')], ''))
-    instance.add(i4 = InstanceItem.new('host4', [RoleItem.new('role2', :primary => true)], ''))
+    instance.add(i4 = InstanceItem.new('host4', [RoleItem.new('role2', 'primary' => true)], ''))
     assert_equal 2, instance.for_role('role1').size, 'not finding correct instances for role'
     assert_equal 2, instance.for_role('role2').size, 'not finding correct instances for role'
     assert_equal 1, instance.for_role('role2', {}).size, 'not finding correct instances for role'
     assert_equal i3, instance.for_role('role2', {}).first, 'not finding correct instances for role'
-    assert_equal 1, instance.for_role('role2', :primary => true).size, 'not finding correct instances for role'
-    assert_equal i4, instance.for_role('role2', :primary => true).first, 'not finding correct instances for role'
+    assert_equal 1, instance.for_role('role2', 'primary' => true).size, 'not finding correct instances for role'
+    assert_equal i4, instance.for_role('role2', 'primary' => true).first, 'not finding correct instances for role'
   end
 end
