b432d8e7ead19d711d9abd3601d393f9b0c98692
Matt Conway <wr0ngway@yahoo.com>
2008-03-31 19:46:40 +0000

    pull in rails_root value
    git-svn-id: svn+ssh://rubyforge.org/var/svn/rubber/trunk@103 04602977-1bcd-48be-894b-5e2845e2d00e

--- generators/vulcanize/templates/minimal_nodb/deploy.rb
+++ generators/vulcanize/templates/minimal_nodb/deploy.rb
@@ -1,5 +1,7 @@
 # This is a sample Capistrano config file for rubber
 
+# pull in basic rails env (RAILS_ROOT)
+require(File.join(File.dirname(__FILE__), 'boot'))
 set :rails_env, (ENV["RAILS_ENV"] ||= 'production')
 set :application, "your_app_name"
 
