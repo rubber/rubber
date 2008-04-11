ae73b672fc2c59a7cbfce7bc15cf04613a715f53
Matt Conway <wr0ngway@yahoo.com>
2008-04-11 20:44:24 +0000

    make it easier to load up rubber within rails environment.rb
    git-svn-id: svn+ssh://rubyforge.org/var/svn/rubber/trunk@112 04602977-1bcd-48be-894b-5e2845e2d00e

---
+++ lib/rubber.rb
@@ -0,0 +1,11 @@
+$:.unshift(File.dirname(__FILE__))
+
+require 'rubber/configuration'
+require 'rubber/environment'
+require 'rubber/generator'
+require 'rubber/instance'
+require 'rubber/util'
+
+module Rubber
+  VERSION = '0.1.0'
+end
