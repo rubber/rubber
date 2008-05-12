a0b9dedcd420e5f65cc3ee7ac2c941f6e8a32bea
Matt Conway <wr0ngway@yahoo.com>
2008-05-12 23:11:56 +0000

    add nginx fair proxy capability
    git-svn-id: svn+ssh://rubyforge.org/var/svn/rubber/trunk@130 04602977-1bcd-48be-894b-5e2845e2d00e

--- generators/vulcanize/templates/nginx/deploy-nginx.rb
+++ generators/vulcanize/templates/nginx/deploy-nginx.rb
@@ -1,6 +1,37 @@
 
 namespace :nginx do
+
+  after "rubber:install_packages", "nginx:custom_install"
   
+  task :custom_install, :roles => :web do
+    # install nginx+fair_proxy over the already installed nginx ubuntu package
+    # this way we get all the system scripts
+    if rubber_cfg.environment.bind().nginx_use_fair_proxy
+      rubber.run_script 'install_nginx', <<-ENDSCRIPT
+        if test -f /usr/sbin/nginx && ! strings /usr/sbin/nginx | grep -c fair > /dev/null; then
+          rm -rf /tmp/*nginx*
+          wget -qP /tmp http://sysoev.ru/nginx/nginx-0.5.35.tar.gz
+          wget -qP /tmp http://github.com/gnosek/nginx-upstream-fair/tarball/master
+          tar -C /tmp -xzf /tmp/nginx-0.5.35.tar.gz
+          tar -C /tmp -xzf /tmp/*nginx-upstream-fair*.tar.gz
+          rm -f /tmp/*nginx*.tar.gz
+          cd /tmp/nginx-0.5.35
+          ./configure --conf-path=/etc/nginx/nginx.conf \
+            --error-log-path=/var/log/nginx/error.log --pid-path=/var/run/nginx.pid \
+            --lock-path=/var/lock/nginx.lock   --http-log-path=/var/log/nginx/access.log \
+            --http-client-body-temp-path=/var/lib/nginx/body --http-proxy-temp-path=/var/lib/nginx/proxy \
+            --http-fastcgi-temp-path=/var/lib/nginx/fastcgi --with-debug --with-http_stub_status_module \
+            --with-http_flv_module --with-http_ssl_module --with-http_dav_module \
+            --prefix=/usr --add-module=/tmp/*nginx-upstream-fair*
+          make
+          test ! -f '/usr/sbin/nginx' || mv '/usr/sbin/nginx' '/usr/sbin/nginx.old'
+          ! killall nginx
+          cp -f objs/nginx /usr/sbin/nginx
+        fi
+      ENDSCRIPT
+    end
+  end
+
   # serial_task can only be called after roles defined - not normally a problem, but
   # rubber auto-roles don't get defined till after all tasks are defined
   on :load do
