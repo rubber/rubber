7011b75ea84505402a868c79579203c90e722457
Matt Conway <wr0ngway@yahoo.com>
2008-05-22 16:48:35 +0000

    fix bootstrapping
    git-svn-id: svn+ssh://rubyforge.org/var/svn/rubber/trunk@159 04602977-1bcd-48be-894b-5e2845e2d00e

---
+++ generators/vulcanize/templates/mysql_cluster/rubber/common/mysql_cluster_migrations.rb
@@ -0,0 +1,13 @@
+<%
+  @path = "#{RAILS_ROOT}/config/initializers/mysql_cluster_migrations.rb"
+%>
+
+# mysql adapter in rails hardcodes engine to be innodb, so if we want all
+# rails tables to be clustered, we need to override this behavior
+#
+class ActiveRecord::ConnectionAdapters::MysqlAdapter
+  def create_table(table_name, options = {}) #:nodoc:
+    super(table_name, options.reverse_merge(:options => "ENGINE=ndbcluster"))
+  end
+end
+
