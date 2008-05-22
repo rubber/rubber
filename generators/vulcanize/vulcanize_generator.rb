b36ec71b77d57902658350ce3f3865d8daa50e9e
Matt Conway <wr0ngway@yahoo.com>
2008-05-22 04:09:54 +0000

    better usage
    git-svn-id: svn+ssh://rubyforge.org/var/svn/rubber/trunk@155 04602977-1bcd-48be-894b-5e2845e2d00e

--- generators/vulcanize/vulcanize_generator.rb
+++ generators/vulcanize/vulcanize_generator.rb
@@ -3,6 +3,9 @@ require 'find'
 
 class VulcanizeGenerator < Rails::Generator::NamedBase
   
+  TEMPLATE_ROOT = File.dirname(__FILE__) + "/templates"
+  TEMPLATE_FILE = "templates.yml"
+    
   def manifest
     record do |m|
       apply_template(m, file_name)
@@ -16,8 +19,7 @@ class VulcanizeGenerator < Rails::Generator::NamedBase
       raise Rails::Generator::UsageError.new("Invalid template #{name}, use one of #{valid_templates.join(', ')}")
     end
     
-    templ_file = "#{sp}templates.yml"
-    templ_conf = YAML.load(File.read(templ_file)) rescue {}
+    templ_conf = load_template_config(sp)
     deps = templ_conf['dependent_templates'] || []
     deps.each do |dep|
       apply_template(m, dep)
@@ -25,7 +27,7 @@ class VulcanizeGenerator < Rails::Generator::NamedBase
     
     Find.find(sp) do |f|
       Find.prune if File.basename(f) =~ /^(CVS|\.svn)$/
-      Find.prune if f == templ_file
+      Find.prune if f == "#{sp}#{TEMPLATE_FILE}"
       rel = f.gsub(/#{source_root}\//, '')
       dest_rel = "config/" + rel.gsub(/#{name}\//, '')
       m.directory(dest_rel) if File.directory?(f)
@@ -35,10 +37,23 @@ class VulcanizeGenerator < Rails::Generator::NamedBase
 
   protected
     def valid_templates
-      valid = Dir.entries(File.dirname(__FILE__) + "/templates").delete_if {|e| e =~ /(^\.)|svn|CVS|Capfile/ }
+      valid = Dir.entries(TEMPLATE_ROOT).delete_if {|e| e =~ /(^\.)|svn|CVS|Capfile/ }
+    end
+    
+    def load_template_config(template_dir)
+      templ_file = "#{template_dir}/templates.yml"
+      templ_conf = YAML.load(File.read(templ_file)) rescue {}
+      return templ_conf
     end
 
     def banner
-      "Usage: #{$0} vulcanize template_name\n\twhere template_name is one of #{valid_templates.join(', ')}"
+      usage = "Usage: #{$0} vulcanize template_name\n"
+      usage << "where template_name is one of:\n\n"
+      valid_templates.each do |t|
+        templ_conf = load_template_config("#{TEMPLATE_ROOT}/#{t}")
+        desc = templ_conf['description']
+        usage << "    #{t}: #{desc}\n" 
+      end
+      return usage
     end
 end
