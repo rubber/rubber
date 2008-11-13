b7b3280681ab9fd66b31bd0964d869621649fb3d
Matt Conway <wr0ngway@yahoo.com>
2008-11-13 15:01:33 -0500

    tweaks to web_tools role

--- generators/vulcanize/templates/nginx/config/rubber/deploy-nginx.rb
+++ generators/vulcanize/templates/nginx/config/rubber/deploy-nginx.rb
@@ -20,10 +20,10 @@ namespace :rubber do
     # serial_task can only be called after roles defined - not normally a problem, but
     # rubber auto-roles don't get defined till after all tasks are defined
     on :load do
-      rubber.serial_task self, :serial_restart, :roles => :web do
+      rubber.serial_task self, :serial_restart, :roles => [:web, :web_tools] do
         run "/etc/init.d/nginx restart"
       end
-      rubber.serial_task self, :serial_reload, :roles => :web do
+      rubber.serial_task self, :serial_reload, :roles => [:web, :web_tools] do
         run "if ! ps ax | grep -v grep | grep -c nginx &> /dev/null; then /etc/init.d/nginx start; else /etc/init.d/nginx reload; fi"
       end
     end
@@ -33,22 +33,22 @@ namespace :rubber do
     after "deploy:restart", "rubber:nginx:reload"
     
     desc "Stops the nginx web server"
-    task :stop, :roles => :web, :on_error => :continue do
+    task :stop, :roles => [:web, :web_tools], :on_error => :continue do
       run "/etc/init.d/nginx stop"
     end
     
     desc "Starts the nginx web server"
-    task :start, :roles => :web do
+    task :start, :roles => [:web, :web_tools] do
       run "/etc/init.d/nginx start"
     end
     
     desc "Restarts the nginx web server"
-    task :restart, :roles => :web do
+    task :restart, :roles => [:web, :web_tools] do
       serial_restart
     end
   
     desc "Reloads the nginx web server"
-    task :reload, :roles => :web do
+    task :reload, :roles => [:web, :web_tools] do
       serial_reload
     end
   
