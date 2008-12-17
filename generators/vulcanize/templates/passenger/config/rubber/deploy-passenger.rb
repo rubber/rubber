77c440f1a49f2d7f413885c9edd137a7e9afa4ff
beat <beat@simplificator.com>
2008-12-18 00:41:26 +0800

    fixed passenger for rubber default ubuntu image
    - execute passenger install after installing rubygems
    - install sound ubuntu apache2 packages and required gems
    - adapted pathnames where needed
    - enable site when deploying
    - fetch current head of http://github.com/FooBarWidget/passenger instead of no
      more available release
    - execute ruby install in unique temporary directory
    
    Signed-off-by: Matt Conway <wr0ngway@yahoo.com>

--- generators/vulcanize/templates/passenger/config/rubber/deploy-passenger.rb
+++ generators/vulcanize/templates/passenger/config/rubber/deploy-passenger.rb
@@ -5,16 +5,22 @@ namespace :rubber do
   
     rubber.allow_optional_tasks(self)
   
-    after "rubber:install_packages", "rubber:passenger:custom_install"
+    after "rubber:base:install_rubygems", "rubber:passenger:custom_install"
     
     task :custom_install, :roles => :web do
       rubber.sudo_script 'install_passenger', <<-ENDSCRIPT
-        wget -qP /tmp http://github.com/tarballs/FooBarWidget-passenger-151a33cc11d8753f5bd4cb0ec2cfee5008dbd840.tar.gz
-        tar -C /tmp -xzf /tmp/FooBarWidget-passenger-151a33cc11d8753f5bd4cb0ec2cfee5008dbd840.tgz
+        TMPDIR=`mktemp -d` || exit 1
+        cd $TMPDIR
+        # download and install current HEAD of passenger
+        wget -q --output-document=passenger.tgz http://github.com/FooBarWidget/passenger/tarball/master
+        tar -xvf passenger.tgz --strip-components 1
         rake package:gem && gem install pkg/*gem --no-rdoc --no-ri
         echo -en "\n\n\n\n" | passenger-install-apache2-module
-        wget -qP /tmp http://rubyforge.org/frs/download.php/41041/ruby-enterprise_1.8.6-20080810-i386.deb
-        dpkg -i /tmp/ruby-enterprise_1.8.6-20080810-i386.deb
+        wget -q http://rubyforge.org/frs/download.php/41041/ruby-enterprise_1.8.6-20080810-i386.deb
+        dpkg -i ruby-enterprise_1.8.6-20080810-i386.deb
+        # enable needed apache modules / disable ubuntu default site
+        a2enmod rewrite
+        a2dissite 000-default
       ENDSCRIPT
     end
   end
