2aa0f6caf40b0d4663c55fdb54b347cce6caed6f
Matt Conway <wr0ngway@yahoo.com>
2009-04-16 16:31:11 -0400

    fixes to sphinx deploy/restart

--- generators/vulcanize/templates/sphinx/config/rubber/deploy-sphinx.rb
+++ generators/vulcanize/templates/sphinx/config/rubber/deploy-sphinx.rb
@@ -10,11 +10,11 @@
 namespace :rubber do
 
   namespace :sphinx do
-  
+
     rubber.allow_optional_tasks(self)
-  
+
     after "rubber:install_packages", "rubber:sphinx:custom_install"
-    
+
     task :custom_install, :roles => :sphinx do
       # install sphinx from source
       ver = "0.9.8.1"
@@ -22,12 +22,12 @@ namespace :rubber do
         # check if already installed
         if [ -x /usr/local/bin/searchd ]
           then echo 'Found sphinx searchd on system'
-          if /usr/local/bin/searchd --help | grep 'Sphinx #{ver}' 
+          if /usr/local/bin/searchd --help | grep 'Sphinx #{ver}'
             then echo 'Sphinx version matches, no further steps needed'
             exit 0
           fi
         fi
-        
+
         echo 'Installing / Upgrading sphinx #{ver}'
         TMPDIR=`mktemp -d` || exit 1
         cd $TMPDIR
@@ -42,18 +42,18 @@ namespace :rubber do
         cd ; rm -rf $TMPDIR
       ENDSCRIPT
     end
-  
+
     set :sphinx_root, Proc.new {"#{shared_path}/sphinx"}
     after "deploy:setup", "rubber:sphinx:setup"
     after "deploy:symlink", "rubber:sphinx:config_dir"
-    
+
     before "deploy:stop", "rubber:sphinx:stop"
     after "deploy:start", "rubber:sphinx:start"
     after "deploy:restart", "rubber:sphinx:restart"
     before "deploy:cold" do
       before "rubber:sphinx:start", "rubber:sphinx:index"
     end
-  
+
     desc "Do sphinx setup tasks"
     task :setup, :roles => :sphinx do
       # Setup links to sphinx config/index as they need to persist between deploys
@@ -68,41 +68,40 @@ namespace :rubber do
 
     # runs the given ultrasphinx rake tasks
     def run_sphinx task
-      cmd = "cd #{current_path} && sudo -u #{runner} RAILS_ENV=#{RAILS_ENV} rake ts:"
-      run cmd+task
+      cmd = "cd #{current_path} && sudo -u #{runner} RAILS_ENV=#{RAILS_ENV} rake #{task}"
+      run cmd
     end
 
-    
+
     desc "Stops sphinx searchd"
     task :stop, :roles => :sphinx, :on_error => :continue do
-      run_sphinx 'stop'
+      run_sphinx 'ts:stop'
     end
-    
+
     desc "Starts sphinx searchd"
     task :start, :roles => :sphinx do
-      run_sphinx 'run'
+      # rake tasks that load rails env can be slow, so
+      # do multiple here as a performance tweak
+      # config always needs to be run before start as
+      # rubber generates a sphinx config file with new paths
+      run_sphinx 'ts:config ts:start'
     end
-    
+
     desc "Restarts sphinx searchd"
     task :restart, :roles => :sphinx do
-      run_sphinx 'stop'
-      run_sphinx 'start'
-    end
-    
-    desc "Configures and builds sphinx index.  Not needed in recent sphinx build as index triggers config"
-    task :rebuild, :roles => :sphinx do
-      run_sphinx 'config'
-      run_sphinx 'index'
+      # rake tasks that load rails env can be slow, so
+      # do multiple here as a performance tweak
+      run_sphinx 'ts:config ts:stop ts:start'
     end
 
     desc "Configures sphinx index"
     task :config, :roles => :sphinx do
-      run_sphinx 'config'
+      run_sphinx 'ts:config'
     end
 
     desc "Builds sphinx index"
     task :index, :roles => :sphinx do
-      run_sphinx 'index'
+      run_sphinx 'ts:index'
     end
 
   end
