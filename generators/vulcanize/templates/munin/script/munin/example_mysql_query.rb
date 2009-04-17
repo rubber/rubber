46440c86583adf9ee53db6b918d660460f6d75b5
Matt Conway <wr0ngway@yahoo.com>
2009-04-17 17:22:08 -0400

    added an easy way to create/install munin plugins

---
+++ generators/vulcanize/templates/munin/script/munin/example_mysql_query.rb
@@ -0,0 +1,55 @@
+#!/usr/bin/env ruby
+#
+# Example munin plugin for querying stats out of db
+# See munin plugin docs http://munin.projects.linpro.no/wiki/Documentation#Plugins
+#
+# Scripts added to RAILS_ROOT/script/munin automatically
+# get installed as munin plugins by rubber
+
+# remove this line to enable
+exit 0
+
+# load just rubber config without loading rails (you can load rails
+# if you want, but may be slow if your app loads a lot of plugins)
+ENV["RAILS_ENV"] ||= "development"
+RAILS_ROOT = File.join(File.dirname(__FILE__), '../..')
+require "#{RAILS_ROOT}/vendor/plugins/rubber/lib/rubber"
+RUBBER_ENV = Rubber::Configuration.rubber_env
+RUBBER_INSTANCES = Rubber::Configuration.rubber_instances
+
+# Print config info need by munin for generating graphs
+if ARGV[0] == "config"
+  puts 'graph_title User Count'
+  puts 'graph_vlabel Users'
+  puts 'graph_category Examples'
+  puts 'graph_scale no'
+  puts 'tusers.label total users'
+  puts 'tusers.type GAUGE'
+  puts 'nusers.label user delta'
+  puts 'nusers.type COUNTER'
+  exit 0
+end
+
+
+query = <<-EOF
+  select count(id) from users;
+EOF
+
+# pick a non-critical db if possible
+source = RUBBER_INSTANCES.for_role("mysql_util").first
+source ||= RUBBER_INSTANCES.for_role("mysql_slave").first
+source ||= RUBBER_INSTANCES.for_role("mysql_master").first
+db_host = source ? source.full_name : 'localhost'
+
+command = "mysql -u #{RUBBER_ENV.db_slave_user}"
+command << " --password=#{RUBBER_ENV.db_pass}"
+command << " -h #{db_host}"
+command << " #{RUBBER_ENV.db_name} --skip-column-names"
+
+# execute a sql query to get some data
+data = `echo "#{query}" | #{command}`
+fail "Couldn't execute command" if $?.exitstatus > 0
+
+# print graph data values
+puts "tusers.value #{data}"
+puts "nusers.value #{data}"
---
+++ generators/vulcanize/templates/munin/script/munin/example_simple.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby
+#
+# Example munin plugin.
+# See munin plugin docs http://munin.projects.linpro.no/wiki/Documentation#Plugins
+#
+# Scripts added to RAILS_ROOT/script/munin automatically
+# get installed as munin plugins by rubber
+
+# remove this line to enable
+exit 0
+
+# Print config info need by munin for generating graphs
+if ARGV[0] == "config"
+  puts 'graph_title Example Stats'
+  puts 'graph_vlabel random 1-100'
+  puts 'graph_category Examples'
+  puts 'graph_scale no'
+  puts 'rand.label random'
+
+  exit 0
+end
+
+# print graph data values
+puts "rand.value #{rand(100)}"
