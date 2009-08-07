4ffbafe158a16233c370db6eac7c795644791787
Charlie H <charlie@snapmylife.com>
2009-08-07 16:12:01 -0400

    Fixed passenger template.

--- generators/vulcanize/templates/passenger/config/rubber/deploy-passenger.rb
+++ generators/vulcanize/templates/passenger/config/rubber/deploy-passenger.rb
@@ -5,16 +5,15 @@ namespace :rubber do
   
     rubber.allow_optional_tasks(self)
   
-    after "rubber:base:install_rubygems", "rubber:passenger:custom_install"
+    after "rubber:install_gems", "rubber:passenger:custom_install"
     
     task :custom_install, :roles => :web do
       rubber.sudo_script 'install_passenger', <<-ENDSCRIPT
         echo -en "\n\n\n\n" | passenger-install-apache2-module
         wget -q http://rubyforge.org/frs/download.php/58679/ruby-enterprise_1.8.6-20090610_i386.deb
         dpkg -i ruby-enterprise_1.8.6-20090610_i386.deb
-        # enable needed apache modules / disable ubuntu default site
-        #a2enmod rewrite
-        #a2dissite default
+        # disable ubuntu default site
+        a2dissite default
       ENDSCRIPT
     end
     
