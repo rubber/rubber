52ed22faa277e5b7e56953f866d34f88dc292627
Matt Conway <wr0ngway@yahoo.com>
2009-09-16 11:24:00 -0400

    comment out inprogress elb code

--- lib/rubber/recipes/rubber/load_balancers.rb
+++ lib/rubber/recipes/rubber/load_balancers.rb
@@ -1,21 +1,38 @@
 namespace :rubber do
 
-  desc <<-DESC
-    Sets up the network load balancers
-  DESC
-  required_task :setup_load_balancers do
-    setup_load_balancers()
-  end
-
-  desc <<-DESC
-    Describes the network load balancers
-  DESC
-  required_task :describe_load_balancers do
-    lbs = cloud.describe_load_balancers()
-    pp lbs
-  end
+  #desc <<-DESC
+  #  Sets up the network load balancers
+  #DESC
+  #required_task :setup_load_balancers do
+  #  setup_load_balancers()
+  #end
+  #
+  #desc <<-DESC
+  #  Describes the network load balancers
+  #DESC
+  #required_task :describe_load_balancers do
+  #  lbs = cloud.describe_load_balancers()
+  #  pp lbs
+  #end
 
   def setup_load_balancers
+    # OPTIONAL: Automatically provision and assign instances to a Cloud provided
+    # load balancer.
+    #load_balancers:
+    #  my_lb_name:
+    #    listeners:
+    #      - protocol: http
+    #        port: 80
+    #        instance_port: 8080
+    #      - protocol: tcp
+    #        port: 443
+    #        instance_port: 8080
+    #    target_roles: [app]
+    #
+    #isolate_load_balancers: true
+
+
+
     # get remote lbs
     # for each local not in remote, add it
     #   get all zones for all instances for roles, and make sure in lb
