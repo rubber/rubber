bb3a6b283ef8f33d83892eb068d69e66b6d8ecad
Matt Conway <wr0ngway@yahoo.com>
2009-11-02 11:46:53 -0500

    initial redis module

---
+++ generators/vulcanize/templates/redis/config/rubber/deploy-redis.rb
@@ -0,0 +1,36 @@
+
+namespace :rubber do
+
+  namespace :redis do
+
+    rubber.allow_optional_tasks(self)
+
+    after "rubber:install_packages", "rubber:redis:custom_install"
+
+    task :custom_install, :roles => :redis do
+      custom_package('http://ftp.us.debian.org/debian/pool/main/r/redis/', 'redis-server', '1.01-1', '! -x /usr/bin/redis-server')
+    end
+
+    desc "Stops the redis server"
+    task :stop, :roles => :redis, :on_error => :continue do
+      run "/etc/init.d/redis-server stop"
+    end
+
+    desc "Starts the redis server"
+    task :start, :roles => :redis do
+      run "/etc/init.d/redis-server start"
+    end
+
+    desc "Restarts the redis server"
+    task :restart, :roles => :redis do
+      run "/etc/init.d/redis-server restart"
+    end
+
+    desc "Reloads the redis server"
+    task :reload, :roles => :redis do
+      run "/etc/init.d/redis-server restart"
+    end
+
+  end
+
+end
