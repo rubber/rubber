0f32f2cce4013baa318f3df02beecbcb8e3c03b6
Kevin Menard <nirvdrum@gmail.com>
2010-05-21 20:17:08 -0400

    Fixed issues in using rubber for non-Rails projects.

--- lib/generators/vulcanize/vulcanize_generator.rb
+++ lib/generators/vulcanize/vulcanize_generator.rb
@@ -167,12 +167,6 @@ else
     def load_template_config(template_dir)
       YAML.load(File.read(File.join(template_dir, 'templates.yml'))) rescue {}
     end
-
-    def gsub_file(relative_destination, regexp, *args, &block)
-     path = destination_path(relative_destination)
-     content = File.read(path).gsub(regexp, *args, &block)
-     File.open(path, 'wb') { |file| file.write(content) }
-    end
   end
 
 end
