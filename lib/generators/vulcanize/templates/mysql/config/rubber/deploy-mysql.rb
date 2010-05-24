708f0537f73b055066a312d93cb1b0eef2531d96
Kevin Menard <nirvdrum@gmail.com>
2010-05-23 23:38:55 -0400

    Fixed an issue with mysqldump and empty passwords.

--- lib/generators/vulcanize/templates/mysql/config/rubber/deploy-mysql.rb
+++ lib/generators/vulcanize/templates/mysql/config/rubber/deploy-mysql.rb
@@ -80,7 +80,7 @@ namespace :rubber do
               logger.info "Creating slave from a dump of master #{source_host}"
               rubber.sudo_script "create_slave_db_from_master", <<-ENDSCRIPT
                 mysql -u root -e "change master to master_host='#{master_host}', master_user='#{env.db_replicator_user}' #{master_pass}"
-                mysqldump -u #{env.db_user} --password #{env.db_pass} -h #{source_host} --all-databases --master-data=1 | mysql -u root
+                mysqldump -u #{env.db_user} #{env.db_pass.nil? ? '' : '--password ' + env.db_pass} -h #{source_host} --all-databases --master-data=1 | mysql -u root
               ENDSCRIPT
             else
               logger.info "Creating slave from a dump of slave #{source_host}"
