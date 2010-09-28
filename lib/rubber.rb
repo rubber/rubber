73c47a8501e6f530cabed65a565d3b7f6b99df11
Kevin Menard <nirvdrum@gmail.com>
2010-09-27 21:39:29 -0400

    Fixed rake gems:install issue with Rails 2.

--- lib/rubber.rb
+++ lib/rubber.rb
@@ -12,6 +12,15 @@ module Rubber
     Object.const_set('RUBBER_ENV', project_env)
     Object.const_set('RUBBER_ROOT', File.expand_path(project_root))
 
+    if ! defined?(Rails) && ! Rubber::Util::is_bundler?
+      # pull in basic rails env.  rubber only needs RAILS_ROOT and RAILS_ENV.
+      # We actually do NOT want the entire rails environment because it
+      # complicates bootstrap (i.e. can't run config to create db because full
+      # rails env needs db to exist as some plugin accesses model or something)
+      rails_boot_file = File.join(RUBBER_ROOT, 'config', 'boot.rb')
+      require(rails_boot_file) if File.exists? rails_boot_file
+    end
+
     if defined?(Rails.logger) && Rails.logger
       @@logger = Rails.logger
     else
@@ -59,9 +68,3 @@ require 'rubber/instance'
 require 'rubber/util'
 require 'rubber/cloud'
 require 'rubber/dns'
-
-if Rubber::Util::is_rails3?
-  module Rubber
-    require 'rubber/railtie'
-  end
-end
