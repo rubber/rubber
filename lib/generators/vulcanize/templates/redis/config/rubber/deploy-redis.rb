e340091a9bda0140cb67e605172c60f56412fcd6
Kevin Menard <nirvdrum@gmail.com>
2010-11-21 00:03:12 -0500

    Support installation of redis 2.0.

--- lib/generators/vulcanize/templates/redis/config/rubber/deploy-redis.rb
+++ lib/generators/vulcanize/templates/redis/config/rubber/deploy-redis.rb
@@ -4,17 +4,56 @@ namespace :rubber do
 
     rubber.allow_optional_tasks(self)
 
+    before "rubber:install_packages", "rubber:redis:setup_apt_sources"
+
+    task :setup_apt_sources, :roles => :redis do
+      rsudo "add-apt-repository ppa:soren/nova"
+    end
+
     after "rubber:install_packages", "rubber:redis:custom_install"
 
     task :custom_install, :roles => :redis do
-      custom_package('http://ftp.us.debian.org/debian/pool/main/r/redis/', 'redis-server', '1.2.6-1', '! -x /usr/bin/redis-server')
-      # force redis config regen so db dir gets recreated when creating staging from a bundle
-      rsudo "rm -f /etc/redis/redis.conf"
+      rubber.sudo_script 'install_redis', <<-ENDSCRIPT
+        if ! redis-server --version | grep "#{rubber_env.redis_server_version}" &> /dev/null; then
+          # Fetch the sources.
+          wget http://redis.googlecode.com/files/redis-#{rubber_env.redis_server_version}.tar.gz
+          tar -zxf redis-#{rubber_env.redis_server_version}.tar.gz
+
+          # Build the binaries.
+          cd redis-#{rubber_env.redis_server_version}
+          make
+
+          # Install the binaries.
+          /etc/init.d/redis-server stop
+
+          mv redis-benchmark /usr/bin/
+          mv redis-check-aof /usr/bin/
+          mv redis-check-dump /usr/bin/
+          mv redis-cli /usr/bin/
+          mv redis-server /usr/bin/
+
+          /etc/init.d/redis-server start
+
+          # Clean up after ourselves.
+          cd ..
+          rm -rf redis-#{rubber_env.redis_server_version}
+          rm redis-#{rubber_env.redis_server_version}.tar.gz
+        fi
+      ENDSCRIPT
+    end
+
+    after "rubber:bootstrap", "rubber:redis:bootstrap"
+
+    task :bootstrap, :roles => :redis do
+      rubber.sudo_script 'bootstrap_redis', <<-ENDSCRIPT
+        mkdir -p #{rubber_env.redis_db_dir}
+        chown -R redis:redis #{rubber_env.redis_db_dir}
+      ENDSCRIPT
     end
 
     desc "Stops the redis server"
-    task :stop, :roles => :redis do
-      rsudo "/etc/init.d/redis-server stop; exit 0"
+    task :stop, :roles => :redis, :on_error => :continue do
+      rsudo "/etc/init.d/redis-server stop"
     end
 
     desc "Starts the redis server"
