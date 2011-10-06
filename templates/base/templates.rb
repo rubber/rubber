810a0a796beb8ab5ea1259c8187d99b0c3a7d8cb
Matt Conway <wr0ngway@yahoo.com>
2011-10-06 13:26:15 -0400

    make sure open4 gets bundled

--- templates/base/templates.rb
+++ templates/base/templates.rb
@@ -1,4 +1,8 @@
-gem "rubber", Rubber.version if Rubber::Util::is_bundler? && ! Rubber::Util::rubber_as_plugin?
+if Rubber::Util::is_bundler?
+  gem "rubber", Rubber.version if ! Rubber::Util::rubber_as_plugin?
+  # for cron-sh
+  gem "open4"
+end
 
 if Rubber::Util::is_rails2?
   m.gsub_file('script/cron-runner', /RAILS_RUNNER/, 'script/runner')
