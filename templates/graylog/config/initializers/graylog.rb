daea035735c00093a28a16a52c128ce646b5d28a
Matt Conway <wr0ngway@yahoo.com>
2012-01-26 11:06:28 -0500

    add graylog2-resque for logging resque failures, use forked graylog2-exceptions, setup Rails.logger to log to disk as well as graylog

--- templates/graylog/config/initializers/graylog.rb
+++ templates/graylog/config/initializers/graylog.rb
@@ -4,11 +4,22 @@ graylog_server = Rubber.instances.for_role('graylog_server').first
 
 if graylog_server
 
-  Rails.logger = GELF::Logger.new(graylog_server.full_name,
-                                  Rubber.config.graylog_server_port,
-                                  'LAN',
-                                  'facility' => 'rails',
-                                  'host' => Rubber.config.host)
+  class MultiLogger
+    def initialize(*objects)
+      @objects = objects
+    end
+
+    def method_missing(*args)
+      @objects.each {|o| o.send(*args) }
+    end
+  end
+  
+  gelf_logger = GELF::Logger.new(graylog_server.full_name,
+                                 Rubber.config.graylog_server_port,
+                                 'LAN',
+                                 'facility' => 'rails',
+                                 'host' => Rubber.config.host)
+  Rails.logger = MultiLogger.new(Rails.logger, gelf_logger)
 
   # See https://github.com/Graylog2/graylog2_exceptions/wiki
   Rails.application.config.middleware.use "Graylog2Exceptions",
@@ -20,5 +31,23 @@ if graylog_server
                                             :level => GELF::FATAL,
                                             :max_chunk_size => 'LAN'
                                           }
-  
+
+  # Setup logging unhandled resque exceptions to graylog
+  Graylog2::Resque::FailureHandler.configure do |config|
+    config.gelf_server = graylog_server.full_name
+    config.gelf_port = Rubber.config.graylog_server_port
+    config.host = Rubber.config.host
+    config.facility = "resque_exceptions"
+    config.level = GELF::FATAL
+    config.max_chunk_size = 'LAN'
+  end
+ 
+  require 'resque/failure/multiple'
+  require 'resque/failure/redis'
+  Resque::Failure::Multiple.classes = [
+      Graylog2::Resque::FailureHandler,
+      Resque::Failure::Redis
+  ]
+  Resque::Failure.backend = Resque::Failure::Multiple
+
 end
--- templates/graylog/templates.rb
+++ templates/graylog/templates.rb
@@ -1,4 +1,5 @@
 if Rubber::Util::is_bundler?
   append_to_file "Gemfile", "gem 'gelf'\n"
-  append_to_file "Gemfile", "gem 'graylog2_exceptions'\n"
+  append_to_file "Gemfile", "gem 'graylog2_exceptions', :git => 'git://github.com/wr0ngway/graylog2_exceptions.git'\n"
+  append_to_file "Gemfile", "gem 'graylog2-resque'\n"
 end
