4b798a34388d945ee2cc96fe78f89fca2a0588fa
Matt Conway <matt@conwaysplace.com>
2012-03-27 09:58:37 -0400

    load rubber even earlier to allow config to referenced in application.rb

--- lib/rubber/railtie.rb
+++ lib/rubber/railtie.rb
@@ -5,7 +5,7 @@ module Rubber
 
   class Railtie < Rails::Railtie
 
-    initializer "rubber.configure_rails_initialization", :before => :load_config_initializers do
+    config.before_configuration do
       Rubber::initialize(Rails.root, Rails.env)
     end
 
