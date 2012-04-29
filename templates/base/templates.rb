201f51530fbbc0a0821ae696da47c756ed6fafec
Matt Conway <matt@conwaysplace.com>
2012-04-29 10:46:12 -0400

    temp workaround for ssl issue on 12.04

--- templates/base/templates.rb
+++ templates/base/templates.rb
@@ -4,4 +4,8 @@ if Rubber::Util::is_bundler?
   # for cron-sh
   append_to_file 'Gemfile', "gem 'open4'\n"
   
+  # TODO: remove this once 12.04 is fixed
+  # temp workaround for https://github.com/rubygems/rubygems/issues/319
+  gsub_file 'Gemfile', /source (["'])https/, 'source \1http'
+  
 end
