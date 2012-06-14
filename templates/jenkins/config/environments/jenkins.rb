253f26533e9c296b6d8d1693cda5fc18757e3188
Scott Carleton <scott@artsicle.com>
2012-06-14 16:08:41 -0300

    jenkins environment needs to have application name. Couldn't figure out how to print it dynamically.  I doubt something like RUBBER_CONFIG['app_name'].capitalize.constantize would work

--- templates/jenkins/config/environments/jenkins.rb
+++ templates/jenkins/config/environments/jenkins.rb
@@ -1,4 +1,4 @@
-Appleton::Application.configure do
+APP_NAME::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # In the development environment your application's code is reloaded on
