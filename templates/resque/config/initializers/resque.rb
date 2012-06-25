2376db20df190f3a0a48d236b45c65b49ae052ea
Nick Messick <engineering+nick@stageit.com>
2012-06-25 12:15:19 -0700

    rails_env was changed to Rubber.env a while ago I believe

--- templates/resque/config/initializers/resque.rb
+++ templates/resque/config/initializers/resque.rb
@@ -4,7 +4,7 @@ redis_host = 'localhost:6379'
 resque_yml = Rubber.root.to_s + '/config/resque.yml'
 if File.exist? resque_yml
   resque_config = YAML.load_file(resque_yml)
-  redis_host = resque_config[rails_env]
+  redis_host = resque_config[Rubber.env]
 end
 
 Resque.redis = redis_host
