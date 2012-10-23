c14c4ee6cae170714ff1cc6671a836d0c9c10fdb
Michael-Keith Bernard <mkbernard@enthu.se>
2012-10-23 10:13:44 -0700

    Update Sidekiq configuration
    * Redis is not required to use Sidekiq (to support 3rd-party Redis vendors)
    * Use `bundle exec` instead of relying on `bundle install --binstubs`

--- templates/sidekiq/config/rubber/deploy-sidekiq.rb
+++ templates/sidekiq/config/rubber/deploy-sidekiq.rb
@@ -10,18 +10,18 @@ namespace :rubber do
 
     desc "Quiet sidekiq (stop accepting new work)"
     task :quiet, :roles => :sidekiq do
-      rsudo "cd #{current_path} && if [ -f #{current_path}/tmp/pids/sidekiq.pid ]; then ./bin/sidekiqctl quiet #{current_path}/tmp/pids/sidekiq.pid ; fi", :as => rubber_env.app_user
+      rsudo "cd #{current_path} && if [ -f #{current_path}/tmp/pids/sidekiq.pid ]; then bundle exec sidekiqctl quiet #{current_path}/tmp/pids/sidekiq.pid ; fi", :as => rubber_env.app_user
     end
 
     desc "Stop sidekiq"
     task :stop, :roles => :sidekiq do
       # Allow workers up to 60 seconds to finish their processing.
-      rsudo "cd #{current_path} && if [ -f #{current_path}/tmp/pids/sidekiq.pid ]; then ./bin/sidekiqctl stop #{current_path}/tmp/pids/sidekiq.pid 60 ; fi", :as => rubber_env.app_user
+      rsudo "cd #{current_path} && if [ -f #{current_path}/tmp/pids/sidekiq.pid ]; then bundle exec sidekiqctl stop #{current_path}/tmp/pids/sidekiq.pid 60 ; fi", :as => rubber_env.app_user
     end
 
     desc "Start sidekiq"
     task :start, :roles => :sidekiq do
-      rsudo "cd #{current_path} ; nohup ./bin/sidekiq -e #{Rubber.env} -C #{current_path}/config/sidekiq.yml -P #{current_path}/tmp/pids/sidekiq.pid >> #{current_path}/log/sidekiq.log 2>&1 &", :pty => false, :as => rubber_env.app_user
+      rsudo "cd #{current_path} ; nohup bundle exec sidekiq -e #{Rubber.env} -C #{current_path}/config/sidekiq.yml -P #{current_path}/tmp/pids/sidekiq.pid >> #{current_path}/log/sidekiq.log 2>&1 &", :pty => false, :as => rubber_env.app_user
       sleep 45 # Give the workers some time to start up before moving on so monit doesn't try to start as well.
     end
 
