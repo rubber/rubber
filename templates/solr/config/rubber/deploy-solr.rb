c10657837126e6024aaf6e389f6f2fb736f9f36f
charlie <charleetm@gmail.com>
2013-02-22 09:21:24 +0000

    externalised more solr and tomcat download options

--- templates/solr/config/rubber/deploy-solr.rb
+++ templates/solr/config/rubber/deploy-solr.rb
@@ -18,41 +18,40 @@ namespace :rubber do
         upload rubber_env.solr_xml_path, "/tmp/#{rubber_env.solr_xml}"
         upload rubber_env.tarz_config_files, "/tmp/solr_conf.tar.gz"
         rubber.sudo_script 'install_java_solr', <<-ENDSCRIPT
-          if [ ! -d "/usr/lib/jvm/jdk1.7" ]; then
-            echo 'installing oracle java'
-            tar -zxf /tmp/#{rubber_env.jdk} -C /tmp
-            sudo mkdir -p /usr/lib/jvm/jdk1.7
-            mv -f /tmp/jdk1.7.0_10/* /usr/lib/jvm/jdk1.7/
+        if [ ! -d "/usr/lib/jvm/jdk1.7" ]; then
+                    echo 'installing oracle java'
+                    tar -zxf /tmp/#{rubber_env.jdk} -C /tmp
+                    sudo mkdir -p /usr/lib/jvm/jdk1.7
+                    mv -f /tmp/jdk1.7.0_10/* /usr/lib/jvm/jdk1.7/
 
-            echo 'updating java alterlative'
-            update-alternatives --install "/usr/bin/java" "java" "/usr/lib/jvm/jdk1.7/bin/java" 1
-            update-alternatives --install "/usr/bin/javac" "javac" "/usr/lib/jvm/jdk1.7/bin/javac" 1
-            update-alternatives --install "/usr/bin/javaws" "javaws" "/usr/lib/jvm/jdk1.7/bin/javaws" 1
+                    echo 'updating java alterlative'
+                    update-alternatives --install "/usr/bin/java" "java" "/usr/lib/jvm/jdk1.7/bin/java" 1
+                    update-alternatives --install "/usr/bin/javac" "javac" "/usr/lib/jvm/jdk1.7/bin/javac" 1
+                    update-alternatives --install "/usr/bin/javaws" "javaws" "/usr/lib/jvm/jdk1.7/bin/javaws" 1
+                    echo 'installing tomcat'
+                    curl -o /tmp/#{tomcat_tar_file_name} #{rubber_env.tomcat_download_url}
+                    tar -zxf /tmp/#{tomcat_tar_file_name} -C #{rubber_env.tomcat_dest_folder}
+                    rm /tmp/#{tomcat_tar_file_name}
 
-            echo 'installing tomcat'
-            curl -o /tmp/apache-tomcat-7.0.34.tar.gz http://ftp.heanet.ie/mirrors/www.apache.org/dist/tomcat/tomcat-7/v7.0.34/bin/apache-tomcat-7.0.34.tar.gz
-            tar -zxf /tmp/apache-tomcat-7.0.34.tar.gz -C #{rubber_env.tomcat_dest_folder}
-            rm /tmp/apache-tomcat-7.0.34.tar.gz
+                    echo 'installing solr'
+                    curl -o /tmp/#{solr_tar_file_name} #{rubber_env.solr_download_url}
+                    tar -zxf /tmp/#{solr_tar_file_name} -C /tmp
+                    cp /tmp/#{rubber_env.solr_untar_folder_name}/dist/#{rubber_env.solr_untar_folder_name}.war /mnt/#{rubber_env.tomcat_untar_folder_name}/webapps/solr.war
+                    rm -fr /tmp/#{rubber_env.solr_untar_folder_name}*
 
-            echo 'installing solr'
-            curl -o /tmp/apache-solr-4.0.0.tgz http://ftp.heanet.ie/mirrors/www.apache.org/dist/lucene/solr/4.0.0/apache-solr-4.0.0.tgz
-            tar -zxf /tmp/apache-solr-4.0.0.tgz -C /tmp
-            cp /tmp/apache-solr-4.0.0/dist/apache-solr-4.0.0.war /mnt/apache-tomcat-7.0.34/webapps/solr.war
-            rm -fr /tmp/apache-solr-4.0.0*
-
-            echo 'setting up solr'
-            mkdir -p #{rubber_env.solr_home_dest_foler}/solr/data
-            mkdir -p #{rubber_env.solr_home_dest_foler}/solr/#{rubber_env.core_name}
-            tar -zxf /tmp/solr_conf.tar.gz -C /mnt/solr/#{rubber_env.core_name}
-            mv /tmp/#{rubber_env.solr_xml} #{rubber_env.solr_home_dest_foler}/solr
-            rm /tmp/solr_conf.tar.gz
-          fi
-        ENDSCRIPT
+                    echo 'setting up solr'
+                    mkdir -p #{rubber_env.solr_home_dest_foler}/solr/data
+                    mkdir -p #{rubber_env.solr_home_dest_foler}/solr/#{rubber_env.core_name}
+                    tar -zxf /tmp/solr_conf.tar.gz -C /mnt/solr/#{rubber_env.core_name}
+                    mv /tmp/#{rubber_env.solr_xml} #{rubber_env.solr_home_dest_foler}/solr
+                    rm /tmp/solr_conf.tar.gz
+              fi
+              ENDSCRIPT
     end
 
 
     def set_java_opts
-      "export JAVA_OPTS='-server -Xmx#{rubber_env.Xmx} -Dsolr.data.dir=#{rubber_env.solr_home_dest_foler}/solr/data -Dsolr.solr.home=#{rubber_env.solr_home_dest_foler}/solr'"
+      "export JAVA_OPTS='-server -Xmx#{rubber_env.xmx} -Dsolr.data.dir=#{rubber_env.solr_home_dest_foler}/solr/data -Dsolr.solr.home=#{rubber_env.solr_home_dest_foler}/solr'"
     end
 
     desc "start solr"
