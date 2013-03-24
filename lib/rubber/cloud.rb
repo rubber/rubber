5fce5887f39889709ab3ac3c31bec8e6fc57fdff
Kevin Menard <nirvdrum@gmail.com>
2013-03-23 22:01:10 -0400

    Convert provider names to camel-case to handle multi-word providers properly.

--- lib/rubber/cloud.rb
+++ lib/rubber/cloud.rb
@@ -5,7 +5,7 @@ module Rubber
 
     def self.get_provider(provider, env, capistrano)
       require "rubber/cloud/#{provider}"
-      clazz = Rubber::Cloud.const_get(provider.capitalize)
+      clazz = Rubber::Cloud.const_get(Rubber::Util.camelcase(provider))
       provider_env = env.cloud_providers[provider]
       return clazz.new(provider_env, capistrano)
     end
