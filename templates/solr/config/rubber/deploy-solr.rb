a45046dc1038ed82a1d8738da8ea7d245899d4d5
Gabriel Cebrian <gabceb@gmail.com>
2013-04-11 16:38:17 -0700

    Changes to improve solr template
    - Fixed typos on deploy-solr.rb and rubber-solr.rb
    - Removed base template dependency to avoid overwriting the base files (rubber-yml, etc.)
    - Updated jdk and tomcat mirrors
    - Added examples to the variables on rubber-solr.yml

--- templates/solr/config/rubber/deploy-solr.rb
+++ templates/solr/config/rubber/deploy-solr.rb
@@ -22,9 +22,9 @@ namespace :rubber do
             echo 'installing oracle java'
             tar -zxf /tmp/#{rubber_env.jdk} -C /tmp
             sudo mkdir -p /usr/lib/jvm/jdk1.7
-            mv -f /tmp/jdk1.7.0_10/* /usr/lib/jvm/jdk1.7/
+            mv -f /tmp/#{rubber_env.jdk}/* /usr/lib/jvm/jdk1.7/
 
-            echo 'updating java alterlative'
+            echo 'updating java alternative'
             update-alternatives --install "/usr/bin/java" "java" "/usr/lib/jvm/jdk1.7/bin/java" 1
             update-alternatives --install "/usr/bin/javac" "javac" "/usr/lib/jvm/jdk1.7/bin/javac" 1
             update-alternatives --install "/usr/bin/javaws" "javaws" "/usr/lib/jvm/jdk1.7/bin/javaws" 1
@@ -37,14 +37,14 @@ namespace :rubber do
             echo 'installing solr'
             curl -o /tmp/apache-solr-4.0.0.tgz http://ftp.heanet.ie/mirrors/www.apache.org/dist/lucene/solr/4.0.0/apache-solr-4.0.0.tgz
             tar -zxf /tmp/apache-solr-4.0.0.tgz -C /tmp
-            cp /tmp/apache-solr-4.0.0/dist/apache-solr-4.0.0.war /mnt/apache-tomcat-7.0.34/webapps/solr.war
+            cp /tmp/apache-solr-4.0.0/dist/apache-solr-4.0.0.war #{rubber_env.tomcat_dest_folder}/apache-tomcat-7.0.34/webapps/solr.war
             rm -fr /tmp/apache-solr-4.0.0*
 
             echo 'setting up solr'
-            mkdir -p #{rubber_env.solr_home_dest_foler}/solr/data
-            mkdir -p #{rubber_env.solr_home_dest_foler}/solr/#{rubber_env.core_name}
+            mkdir -p #{rubber_env.solr_home_dest_folder}/solr/data
+            mkdir -p #{rubber_env.solr_home_dest_folder}/solr/#{rubber_env.core_name}
             tar -zxf /tmp/solr_conf.tar.gz -C /mnt/solr/#{rubber_env.core_name}
-            mv /tmp/#{rubber_env.solr_xml} #{rubber_env.solr_home_dest_foler}/solr
+            mv /tmp/#{rubber_env.solr_xml} #{rubber_env.solr_home_dest_folder}/solr
             rm /tmp/solr_conf.tar.gz
           fi
         ENDSCRIPT
@@ -52,7 +52,7 @@ namespace :rubber do
 
 
     def set_java_opts
-      "export JAVA_OPTS='-server -Xmx#{rubber_env.Xmx} -Dsolr.data.dir=#{rubber_env.solr_home_dest_foler}/solr/data -Dsolr.solr.home=#{rubber_env.solr_home_dest_foler}/solr'"
+      "export JAVA_OPTS='-server -Xmx#{rubber_env.Xmx} -Dsolr.data.dir=#{rubber_env.solr_home_dest_folder}/solr/data -Dsolr.solr.home=#{rubber_env.solr_home_dest_folder}/solr'"
     end
 
     desc "start solr"
@@ -68,7 +68,7 @@ namespace :rubber do
     desc "stop solr"
     task :stop_solr, :roles => :solr do
       rubber.sudo_script 'stop_solr', <<-ENDSCRIPT
-        echo 'stoping tomcat'
+        echo 'stopping tomcat'
         #{set_java_opts}
         #{rubber_env.tomcat_dest_folder}/apache-tomcat-7.0.34/bin/shutdown.sh
       ENDSCRIPT
