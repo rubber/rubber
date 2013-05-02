8757cf8ace8534d0153beecad0184c10d2d930e0
Johnny Shields <johnny.shields@gmail.com>
2013-05-03 03:12:24 +0900

    Improvements to memcached:
    - Make the following memcached variables configurable
        * port
        * pid file
        * max slab size
        * max memory usage (can be set by AWS instance type)
    
    - Add security group for memcached port
    
    - Change name of memcached user from 'nobody' to 'memcache' as per Ubuntu standard (see: https://bugs.launchpad.net/ubuntu/+source/memcached/+bug/599461)
    
    - Add stop/start/restart/status actions for memcached
    
    - Added Dalli config to share memcached between all instances
    
    - Deleted memcached.yml (it is obsolete and not used by Dalli)
    
    - Merge branch 'monit-memcached' of matsu911/rubber

--- templates/memcached/config/rubber/deploy-memcached.rb
+++ templates/memcached/config/rubber/deploy-memcached.rb
@@ -3,6 +3,22 @@ namespace :rubber do
   
   namespace :memcached do
 
+    desc "Starts memcached shared memory"
+    task :start, :roles => :memcached do
+      rsudo "service memcached status || service memcached start"
+    end
+
+    desc "Stops memcached shared memory"
+    task :stop, :roles => :memcached do
+      rsudo "service memcached stop || true"
+    end
+
+    desc "Restarts memcached shared memory"
+    task :restart, :roles => :memcached do
+      stop
+      start
+    end
+
     desc "Display status of memcached shared memory"
     task :status, :roles => :memcached do
       rsudo "service memcached status || true"
