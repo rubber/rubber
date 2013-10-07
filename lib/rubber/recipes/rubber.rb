rubber/rubber

commit 74d5831310fce5b070ea6f7493e8f88db2f6213f

With the introduction of the generic provider we expanded our options beyond just the Linux and Windows platforms.  So, make sure we only run Linux tasks on the Linux instances.

lib/rubber/recipes/rubber.rb
@@ -13,7 +13,7 @@ namespace :rubber do
   # Disable connecting to any Windows instance.
   alias :original_task :task
   def task(name, options={}, &block)
-    original_task(name, options.merge(:except => { :platform => 'windows' }), &block)
+    original_task(name, options.merge(:only => { :platform => 'linux' }), &block)
   end
 
   # advise capistrano's task method so that tasks for non-existent roles don't
@@ -26,7 +26,7 @@ namespace :rubber do
       alias :required_task :task
       def task(name, options={}, &block)
         # Disable connecting to any Windows instance.
-        required_task(name, options.merge(:except => { :platform => 'windows' })) do
+        required_task(name, options.merge(:only => { :platform => 'linux' })) do
           # define empty roles for the case when a task has a role that we don't define anywhere
           unless options[:roles].respond_to?(:call)
             [*options[:roles]].each do |r|
@@ -57,7 +57,7 @@ namespace :rubber do
     # use a pty so we don't get "stdin: is not a tty" error output
     default_run_options[:pty] = true if default_run_options[:pty].nil?
     default_run_options[:shell] = "/bin/bash -l" if default_run_options[:shell].nil?
-    default_run_options[:except] ||= { :platform => 'windows' }
+    default_run_options[:only] ||= { :platform => 'linux' }
 
     set :cloud, Rubber.cloud(self)
 

lib/rubber/instance.rb
@@ -256,6 +256,14 @@ module Rubber
         @platform || 'linux'
       end
 
+      def linux?
+        platform == 'linux'
+      end
+
+      def mac?
+        platform == 'mac'
+      end
+
       def windows?
         platform == 'windows'
       end

lib/rubber/recipes/rubber/instances.rb
@@ -367,7 +367,7 @@ namespace :rubber do
       instance_item.root_device_type = instance[:root_device_type]
       rubber_instances.save()
 
-      unless instance_item.windows?
+      if instance_item.linux?
         # weird cap/netssh bug, sometimes just hangs forever on initial connect, so force a timeout
         begin
           Timeout::timeout(30) do

commit d3ae2fa542d5bd66434bb727c53b3ab41e49be6c

Hopefully fixing a problem I introduced in 74d5831310fce5b070ea6f7493e8f88db2f6213f whereby I was clobbering any cap task that had an :only option supplied.

lib/rubber/recipes/rubber.rb
@@ -13,7 +13,13 @@ namespace :rubber do
   # Disable connecting to any Windows instance.
   alias :original_task :task
   def task(name, options={}, &block)
-    original_task(name, options.merge(:only => { :platform => 'linux' }), &block)
+    if options.has_key?(:only)
+      options[:only][:platform] = 'linux'
+    else
+      options[:only] = { :platform => 'linux' }
+    end
+
+    original_task(name, options, &block)
   end
 
   # advise capistrano's task method so that tasks for non-existent roles don't
@@ -25,8 +31,13 @@ namespace :rubber do
     class << ns
       alias :required_task :task
       def task(name, options={}, &block)
-        # Disable connecting to any Windows instance.
-        required_task(name, options.merge(:only => { :platform => 'linux' })) do
+        if options.has_key?(:only)
+          options[:only][:platform] = 'linux'
+        else
+          options[:only] = { :platform => 'linux' }
+        end
+
+        required_task(name, options) do
           # define empty roles for the case when a task has a role that we don't define anywhere
           unless options[:roles].respond_to?(:call)
             [*options[:roles]].each do |r|
@@ -57,7 +68,12 @@ namespace :rubber do
     # use a pty so we don't get "stdin: is not a tty" error output
     default_run_options[:pty] = true if default_run_options[:pty].nil?
     default_run_options[:shell] = "/bin/bash -l" if default_run_options[:shell].nil?
-    default_run_options[:only] ||= { :platform => 'linux' }
+
+    if default_run_options.has_key?(:only)
+      default_run_options[:only][:platform] = 'linux'
+    else
+      default_run_options[:only] = { :platform => 'linux' }
+    end
 
     set :cloud, Rubber.cloud(self)
 