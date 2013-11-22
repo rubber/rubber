c56a93c6b4023595e6ed85b851a7e3ea61805e9a
Scott Carleton <scott@artsicle.com>
2013-11-22 14:53:27 -0500

    Unicorn initialization script
    Handles correct starting, restarting, stopping and reloading

--- templates/unicorn/config/rubber/deploy-unicorn.rb
+++ templates/unicorn/config/rubber/deploy-unicorn.rb
@@ -2,33 +2,38 @@
 namespace :rubber do
 
   namespace :unicorn do
-  
+
     rubber.allow_optional_tasks(self)
-    
+
     before "deploy:stop", "rubber:unicorn:stop"
     after "deploy:start", "rubber:unicorn:start"
     after "deploy:restart", "rubber:unicorn:reload"
-    
+
     desc "Stops the unicorn server"
     task :stop, :roles => :unicorn do
-      rsudo "if [ -f /var/run/unicorn.pid ]; then pid=`cat /var/run/unicorn.pid` && kill -TERM $pid; fi"
+      rsudo "service unicorn stop"
     end
-    
+
+    desc "Forcefully kills the unicorn server"
+    task :force_stop, :roles => :unicorn do
+      rsudo "service unicorn force-stop"
+    end
+
     desc "Starts the unicorn server"
     task :start, :roles => :unicorn do
-      rsudo "cd #{current_path} && bundle exec unicorn_rails -c #{current_path}/config/unicorn.rb -E #{Rubber.env} -D"
+      rsudo "service unicorn start"
     end
-    
+
     desc "Restarts the unicorn server"
     task :restart, :roles => :unicorn do
-      stop
-      start
+      rsudo "service unicorn restart"
     end
-  
+
     desc "Reloads the unicorn web server"
     task :reload, :roles => :unicorn do
-      rsudo "if [ -f /var/run/unicorn.pid ]; then pid=`cat /var/run/unicorn.pid` && kill -USR2 $pid; else cd #{current_path} && bundle exec unicorn_rails -c #{current_path}/config/unicorn.rb -E #{Rubber.env} -D; fi"
+      rsudo "service unicorn upgrade"
     end
+
 
     desc "Display status of the unicorn web server"
     task :status, :roles => :unicorn do
