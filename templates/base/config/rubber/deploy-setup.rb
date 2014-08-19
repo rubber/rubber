653bd0c843eee755ebaf27eb712205ccffe8c022
Kevin Menard <kevin@nirvdrum.com>
2014-08-19 18:25:04 -0400

    Added a task for cleaning up old kernels.

--- templates/base/config/rubber/deploy-setup.rb
+++ templates/base/config/rubber/deploy-setup.rb
@@ -104,5 +104,11 @@ namespace :rubber do
     task :reinstall_virtualbox_additions, :only => { :provider => 'vagrant' } do
       rsudo "service vboxadd setup"
     end
+
+    task :cleanup_old_kernels do
+      rubber.sudo_script 'cleanup_old_kernels', <<-ENDSCRIPT
+        dpkg -l 'linux-*' | sed '/^ii/!d;/'"$(uname -r | sed "s/\\(.*\\)-\\([^0-9]\\+\\)/\\1/")"'/d;s/^[^ ]* [^ ]* \\([^ ]*\\).*/\\1/;/[0-9]/!d' | xargs dpkg --purge
+      ENDSCRIPT
+    end
   end
 end
