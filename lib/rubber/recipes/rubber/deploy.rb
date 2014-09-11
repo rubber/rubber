rubber/rubber

commit bd82f2a9c15ddf2bd0d88821149a70b99ac03d7f

Local windows support: - Add dos2unix task for config - Run dos2unix on code only if it was deployed via remote copy - Introduce FIX_LINE_ENDINGS env var to force dos2unix to run - Refactor rubber:config task to have sub tasks :push and :configure, so we can hook dos2unix between the two steps. - Remove use_dos2unix variable (which was introduced in early attempts at Windows support)

lib/rubber/recipes/rubber/deploy.rb
@@ -28,26 +28,36 @@ namespace :rubber do
   after "deploy:rollback_code", "rubber:config"
   before "deploy:migrate", "rubber:config"
 
-  desc <<-DESC
+  namespace :config do
+
+    desc <<-DESC
+    Pushes and runs rubber configuration on the deployed rails application
+    DESC
+    task :default do
+      # Don't want to do rubber:config during bootstrap_db where it's triggered by
+      # deploy:update_code, because the user could be requiring the rails env inside
+      # some of their config templates (which fails because rails can't connect to
+      # the db)
+      if fetch(:rubber_updating_code_for_bootstrap_db, false)
+        logger.info "Updating code for bootstrap, skipping rubber:config"
+      else
+        rubber.config.push
+        rubber.config.configure
+      end
+    end
+
+    desc <<-DESC
+    Pushes instance config and rubber secret file to remote
+    DESC
+    task :push do
+      push_config
+    end
+
+    desc <<-DESC
     Configures the deployed rails application by running the rubber configuration process
-  DESC
-  task :config do
-    # Don't want to do rubber:config during bootstrap_db where it's triggered by
-    # deploy:update_code, because the user could be requiring the rails env inside
-    # some of their config templates (which fails because rails can't connect to
-    # the db)
-    if fetch(:rubber_updating_code_for_bootstrap_db, false)
-      logger.info "Updating code for bootstrap, skipping rubber:config"
-    else
-      opts = {}
-      opts[:no_post] = true if ENV['NO_POST']
-      opts[:force] = true if ENV['FORCE']
-      opts[:file] = ENV['FILE'] if ENV['FILE']
-
-      # when running deploy:migrations, we need to run config against release_path
-      opts[:deploy_path] = current_release if fetch(:migrate_target, :current).to_sym == :latest
-
-      run_config(opts)
+    DESC
+    task :configure do
+      run_config
     end
   end
 
@@ -63,12 +73,7 @@ namespace :rubber do
     rsudo "chown -R #{rubber_env.app_user}:#{rubber_env.app_user} #{current_path}/tmp"
   end
 
-  def run_config(options={})
-    path = options.delete(:deploy_path) || current_path
-    opts = ""
-    opts += " --no_post" if options[:no_post]
-    opts += " --force" if options[:force]
-    opts += " --file=\"#{options[:file]}\"" if options[:file]
+  def push_config
 
     # Need to do this so we can work with staging instances without having to
     # checkin instance file between create and bootstrap, as well as during a deploy
@@ -83,7 +88,7 @@ namespace :rubber do
 
       push_files.each do |file|
         dest_file = file.sub(/^#{Rubber.root}\/?/, '')
-        put(File.read(file), File.join(path, dest_file), :mode => "+r")
+        put(File.read(file), File.join(config_path, dest_file), :mode => "+r")
       end
     end
 
@@ -91,10 +96,21 @@ namespace :rubber do
     secret = rubber_cfg.environment.config_secret
     if secret && File.exist?(secret)
       base = rubber_cfg.environment.config_root.sub(/^#{Rubber.root}\/?/, '')
-      put(File.read(secret), File.join(path, base, File.basename(secret)), :mode => "+r")
+      put(File.read(secret), File.join(config_path, base, File.basename(secret)), :mode => "+r")
     end
-    
-    rsudo "cd #{path} && RUBBER_ENV=#{Rubber.env} RAILS_ENV=#{Rubber.env} ./script/rubber config #{opts}"
+  end
+
+  def run_config
+    opts = ""
+    opts += " --no_post" if ENV['NO_POST']
+    opts += " --force"   if ENV['FORCE']
+    opts += " --file=\"#{ENV['FILE']}\"" if ENV['FILE']
+    rsudo "cd #{config_path} && RUBBER_ENV=#{Rubber.env} RAILS_ENV=#{Rubber.env} ./script/rubber config #{opts}"
+  end
+
+  def config_path
+    # when running deploy:migrations, we need to run config against release_path
+    fetch(:migrate_target, :current).to_sym == :latest ? current_release : current_path
   end
 
 end

lib/rubber/commands/config.rb
@@ -62,7 +62,6 @@ module Rubber
         gen.no_post = no_post?
         gen.force = force?
         gen.stop_on_error_cmd = env.stop_on_error_cmd
-        gen.use_dos2unix = env.use_dos2unix
         gen.run
 
       end

lib/rubber/generator.rb
@@ -15,7 +15,6 @@ module Rubber
       attr_accessor :force
       attr_accessor :fake_root
       attr_accessor :stop_on_error_cmd
-      attr_accessor :use_dos2unix
 
       def initialize(config_dir, roles, host, options={})
         @config_dir = config_dir
@@ -124,7 +123,6 @@ module Rubber
             open(config_path, 'w') do |pipe|
               pipe.write(result)
             end
-            system("dos2unix #{config_path} || true") if use_dos2unix
 
           # Handle write_cmd by dumping the body to a file and then piping that into the command.
           else
@@ -133,7 +131,6 @@ module Rubber
             begin
               file.write(result)
               file.close
-              system("dos2unix #{file.path} || true") if use_dos2unix
 
               system("cat #{file.path} | #{config.write_cmd}")
             ensure

templates/local_windows/config/rubber/deploy-local_windows.rb
@@ -17,19 +17,40 @@ on :load do
 
 end
 
-after "deploy:update_code", "deploy:local_windows:dos2unix_code"
-
-namespace :deploy do
+namespace :rubber do
 
   namespace :local_windows do
 
-    desc <<-DESC
-      Converts Windows-style line endings (CR+LF) to Unix-style (LF)
-      after code has been copied to remote server.
-    DESC
+    # Run dos2unix on code only if it has been deployed via copy
+    if ENV.has_key?('FIX_LINE_ENDINGS') || (Rubber.config.local_windows? && (fetch(:deploy_via, nil) == :copy))
+      after "deploy:update_code", "rubber:local_windows:dos2unix_code"
+    end
 
+    # Always run dos2unix each time config is pushed, as the Rubber secret file is always pushed via copy
+    if ENV.has_key?('FIX_LINE_ENDINGS') || Rubber.config.local_windows?
+      after "rubber:config:push", "rubber:local_windows:dos2unix_config"
+    end
+
+    desc <<-DESC
+      Converts remote code files to Windows-style line endings (CR+LF) to Unix-style (LF)
+    DESC
     task :dos2unix_code, :except => { :platform => 'windows' } do
-      rsudo "find #{release_path} -type f -exec dos2unix -q {} \\;" if rubber_env.use_dos2unix
+      run_dos2unix release_path
+    end
+
+    desc <<-DESC
+      Converts remote config files to Windows-style line endings (CR+LF) to Unix-style (LF)
+    DESC
+    task :dos2unix_config, :except => { :platform => 'windows' } do
+      run_dos2unix config_path
+    end
+
+    def run_dos2unix(path)
+      rsudo "find #{path} -type f -exec dos2unix -q {} \\;"
+    end
+
+    def config_path
+      release_path + rubber_cfg.environment.config_root.sub(/^#{Rubber.root}\/?/, '')
     end
 
   end

templates/local_windows/config/rubber/rubber-local_windows.yml
@@ -1,13 +1,4 @@
 # dos2unix is a utility which converts line-endings from Windows- to Unix-style.
 # It does not change files which are already Unix format.
 #
-# Set use_dos2unix = true if you are ever copying code or config files
-# from Windows to Unix, i.e. look for either of these lines in deploy.rb:
-#
-#   set :deploy_via, :copy
-#
-#   set :push_instance_config, true
-#
-use_dos2unix: true
-
 packages: [dos2unix]

commit 513e0e18c5d9d16234d88e4d79bfa57202378a90

Allow rubber:config execution to be optional again.
This fixes a regression introduced in bd82f2a9c15ddf2bd0d88821149a70b99ac03d7f by converting the rubber:config task to a nested namespace.

The reason this task needs to be optional is we support clusters consisting of machines that can't be deployed to.  In particular, we'll only deploy to Linux machines, currently.  Without this fix in place, any Windows or Mac machines in the cluster will also attempt to be deployed to and will end up failing.

lib/rubber/recipes/rubber/deploy.rb
@@ -30,6 +30,8 @@ namespace :rubber do
 
   namespace :config do
 
+    allow_optional_tasks(self)
+
     desc <<-DESC
     Pushes and runs rubber configuration on the deployed rails application
     DESC