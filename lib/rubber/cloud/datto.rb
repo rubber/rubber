0428f2aa9143fe9046106c8f15ac011b57198b45
james-lawrence <james.lawrence@backupify.com>
2015-03-18 07:54:59 -0400

    wip

--- lib/rubber/cloud/datto.rb
+++ lib/rubber/cloud/datto.rb
@@ -1,13 +1,10 @@
 require 'rubber/cloud/generic'
 require 'net/http'
-require 'pry'
-
 module Rubber
   module Cloud
     class Datto < Base
       def initialize(env, capistrano)
         super(env, capistrano)
-        binding.pry
       end
 
       def endpoint
@@ -23,119 +20,54 @@ module Rubber
       end
 
       def create_instance(instance_alias, image_name, image_type, security_groups, availability_zone, datacenter)
-        binding.pry
-        # if env.domain.nil?
-        #   raise "'domain' value must be configured"
-        # end
+        response = HttpAdapter.post("http://#{self.endpoint}/index.php/worker")
+        if response.code != 200
+          raise StandardError.new(response["result"] || "Unexpected Response Code #{response.code}")
+        end
 
-        # rubber_cfg = Rubber::Configuration.get_configuration(Rubber.env)
-        # host_env = rubber_cfg.environment.bind(nil, instance_alias)
-
-        # if host_env.public_nic.nil? && host_env.private_nic.nil?
-        #   raise "You must configure a private or a public NIC for this host in your rubber YAML"
-        # end
-
-        # if host_env.public_nic && env.public_network_name.nil?
-        #   raise "You must configure the 'public_network_name' in the provider configuration"
-        # end
-
-        # if host_env.private_nic && env.private_network_name.nil?
-        #   raise "You must configure the 'private_network_name' in the provider configuration"
-        # end
-
-        # nics = []
-
-        # if host_env.public_nic
-        #   nic = nic_to_vsphere_config(host_env.public_nic, env.public_nic || {})
-        #   validate_nic_vsphere_config(nic, :public)
-        #   nics << nic
-        # end
-
-        # if host_env.private_nic
-        #   nic = nic_to_vsphere_config(host_env.private_nic, env.private_nic || {})
-        #   validate_nic_vsphere_config(nic, :private)
-        #   nics << nic
-        # end
-
-        # vm_clone_options = {
-        #   'datacenter' => datacenter,
-        #   'template_path' => image_name,
-        #   'name' => instance_alias,
-        #   'power_on' => false
-        # }
-
-        # if host_env.memory
-        #   vm_clone_options['memoryMB'] = host_env.memory
-        # end
-
-        # if host_env.cpus
-        #   vm_clone_options['numCPUs'] = host_env.cpus
-        # end
-
-        # vm = compute_provider.vm_clone(vm_clone_options)
-
-        # server = compute_provider.servers.get(vm['new_vm']['id'])
-
-        # # Destroy all existing NICs.  We need the public and private IPs to line up with the NICs attached to the
-        # # correct virtual switches.  Rather than take the cross-product and try to work that out, it's easier to
-        # # just start fresh and guarantee everything works as intended.
-        # server.interfaces.each(&:destroy)
-
-        # server.interfaces.create(:network => env.public_network_name) if host_env.public_nic
-        # server.interfaces.create(:network => env.private_network_name) if host_env.private_nic
-
-        # vm_ref = compute_provider.send(:get_vm_ref, server.id)
-        # vm_ref.CustomizeVM_Task(:spec => customization_spec(instance_alias, nics))
-
-        # server.start
-
-        # vm['new_vm']['id']
+        response["id"]
       end
 
       def describe_instances(instance_id=nil)
-        binding.pry
-        # response = httpAdapter.get('http://10.30.95.138/index.php/worker')
-        # response["results"]
-      #   output = `vagrant status #{instance_id}`
+        instances = []
+        if instance_id.nil?
+          response = HttpAdapter.get("http://#{self.endpoint}/index.php/worker")
+          if response.code != 200
+            raise StandardError.new(response["result"] || "Unexpected Response Code #{response.code}")
+          end
 
-      #   output =~ /#{instance_id}\s+(\w+)/m
-      #   state = $1
-
-      #   if Generic.instances
-      #     Generic.instances.each do |instance|
-      #       if instance[:id] == instance_id
-      #         instance[:state] = state
-      #         instance[:provider] = 'vagrant'
-      #       end
-      #     end
-
-      #     Generic.instances
-      #   else
-      #     instance = {}
-      #     instance[:id] = instance_id
-      #     instance[:state] = state
-      #     instance[:external_ip] = capistrano.rubber.get_env('EXTERNAL_IP', "External IP address for host '#{instance_id}'", true)
-      #     instance[:internal_ip] = capistrano.rubber.get_env('INTERNAL_IP', "Internal IP address for host '#{instance_id}'", true, instance[:external_ip])
-      #     instance[:provider] = 'vagrant'
-
-      #     [instance]
-      #   end
-
-        # TODO
+          response["results"].each do |item|
+            instance = {}
+            instance[:id] = item["id"]
+            instance[:external_ip] = item["ip"]
+            instance[:internal_ip] = item["ip"]
+            instance[:provider] = 'datto'
+            instances << instance
+          end
+        else
+          response = HttpAdapter.get("http://#{self.endpoint}/index.php/worker/#{instance_id}")
+          if response.code != 200
+            raise StandardError.new(response["result"] || "Unexpected Response Code #{response.code}")
+          end
+          instance = {}
+          instance[:id] = response["id"]
+          instance[:external_ip] = response["ip"]
+          instance[:internal_ip] = response["ip"]
+          instance[:state] = response["workerRunning"] ? active_state : stopped_state
+          instance[:provider] = 'datto'
+          instance[:platform] = Rubber::Platforms::LINUX
+          instances << instance
+        end
+        instances
       end
 
       def destroy_instance(instance_id)
-        # response = httpAdapter.delete("http://10.30.95.138/index.php/worker/#{instance}")
-        binding.pry
+        response = HttpAdapter.delete("http://#{self.endpoint}/index.php/worker/#{instance_id}")
+        if response.code != 200
+          raise StandardError.new(response["result"] || "Unexpected Response Code #{response.code}")
+        end
+        response
       end
-
-      # def stop_instance(instance, force=false)
-      #   system("vagrant suspend #{instance.instance_id}")
-      # end
-
-      # def start_instance(instance)
-      #   system("vagrant resume #{instance.instance_id}")
-      # end
 
       private
 
