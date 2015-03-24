99188a5f3a016aa1902c0e0e0d5775efcadc4673
james-lawrence <james.lawrence@backupify.com>
2015-03-24 08:12:20 -0400

    peer review cleanup

--- lib/rubber/cloud/datto.rb
+++ lib/rubber/cloud/datto.rb
@@ -4,10 +4,6 @@ require 'httparty'
 module Rubber
   module Cloud
     class Datto < Base
-      def initialize(env, capistrano)
-        super(env, capistrano)
-      end
-
       def endpoint
         env.endpoint
       end
@@ -21,29 +17,30 @@ module Rubber
       end
 
       def create_instance(instance_alias, image_name, image_type, security_groups, availability_zone, datacenter)
-        response = HttpAdapter.post(uriBuilder)
+        response = HttpAdapter.post(uri_builder)
         if response.code != 200
-          raise errFromResponse(response)
+          raise err_from_response(response)
         end
 
         response["id"]
       end
 
       def describe_instances(instance_id=nil)
+        response = HttpAdapter.get(uri)
         instances = []
         if instance_id.nil?
-          response = HttpAdapter.get(uriBuilder)
+          response = HttpAdapter.get(uri_builder)
           if response.code != 200
-            raise errFromResponse(response)
+            raise err_from_response(response)
           end
 
           response["results"].each do |item|
             instances << instance_from_response(item)
           end
         else
-          response = HttpAdapter.get(uriBuilder(instance_id))
+          response = HttpAdapter.get(uri_builder(instance_id))
           if response.code != 200
-            raise errFromResponse(response)
+            raise err_from_response(response)
           end
 
           instances << instance_from_response(response)
@@ -52,9 +49,9 @@ module Rubber
       end
 
       def destroy_instance(instance_id)
-        response = HttpAdapter.delete(uriBuilder(instance_id))
+        response = HttpAdapter.delete(uri_builder(instance_id))
         if response.code != 200
-          raise errFromResponse(response)
+          raise err_from_response(response)
         end
         response
       end
@@ -64,27 +61,27 @@ module Rubber
       # @param [Hash] response - response hash representing an instance
       # @param [Hash] converted hash conforming to the rubber instance api.
       def instance_from_response(response)
-          instance = {}
-          instance[:id] = response["id"]
-          instance[:external_ip] = response["ip"]
-          instance[:internal_ip] = response["ip"]
-          instance[:state] = (!!response["workerRunning"]) ? active_state : stopped_state
-          instance[:provider] = 'datto'
-          instance[:platform] = Rubber::Platforms::LINUX
-          instance
+        instance = {}
+        instance[:id] = response["id"]
+        instance[:external_ip] = response["ip"]
+        instance[:internal_ip] = response["ip"]
+        instance[:state] = response["workerRunning"] ? active_state : stopped_state
+        instance[:provider] = "datto"
+        instance[:platform] = Rubber::Platforms::LINUX
+        instance
       end
 
       # @param [HTTPartyResponse] response
       # @return [StandardError]
-      def errFromResponse(response)
+      def err_from_response(response)
         StandardError.new(response["result"] || "Unexpected Response Code #{response.code}")
       end
 
       # Creates the uri from the specified path and the configured endpoint.
       # @param [String|Nil] path
       # @return String - uri
-      def uriBuilder(path = nil)
-        base = "http://#{self.endpoint}/index.php/worker"
+      def uri_builder(path = nil)
+        base = "http://#{endpoint}/index.php/worker"
         path.nil? ? base : File.join(base, path)
       end
 
