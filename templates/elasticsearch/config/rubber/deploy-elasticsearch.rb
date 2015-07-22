c69c0a48f25706bb0bc2c58059de300fa3430dfa
Matt Conway <matt@conwaysplace.com>
2015-07-22 08:06:48 -0400

    update elasticsearch recipe - uses apt repo, adds optional index expiration

--- templates/elasticsearch/config/rubber/deploy-elasticsearch.rb
+++ templates/elasticsearch/config/rubber/deploy-elasticsearch.rb
@@ -4,19 +4,27 @@ namespace :rubber do
 
     rubber.allow_optional_tasks(self)
 
+    before "rubber:install_packages", "rubber:elasticsearch:setup_apt_sources"
+
+    task :setup_apt_sources, :roles => :elasticsearch do
+      rubber.sudo_script 'setup_elasticsearch_apt', <<-ENDSCRIPT
+        if [[ ! -f /etc/apt/sources.list.d/elasticsearch.list ]]; then
+          curl --silent https://packages.elastic.co/GPG-KEY-elasticsearch | apt-key add -
+          echo "deb http://packages.elastic.co/elasticsearch/#{rubber_env.elasticsearch_major_version}/debian stable main" > /etc/apt/sources.list.d/elasticsearch.list
+        fi
+      ENDSCRIPT
+    end
+
     after "rubber:install_packages", "rubber:elasticsearch:install"
 
     task :install, :roles => :elasticsearch do
       rubber.sudo_script 'install_elasticsearch', <<-ENDSCRIPT
-        if [[ ! -f /usr/share/elasticsearch/lib/elasticsearch-#{rubber_env.elasticsearch_version}.jar ]]; then
-          wget -qNP /tmp https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-#{rubber_env.elasticsearch_version}.deb
-          dpkg -i /tmp/elasticsearch-#{rubber_env.elasticsearch_version}.deb
-          rm /tmp/elasticsearch-#{rubber_env.elasticsearch_version}.deb
-
-          rm -rf /usr/share/elasticsearch/plugins/head
-          /usr/share/elasticsearch/bin/plugin -install mobz/elasticsearch-head
+        if [[ ! -d "#{rubber_env.elasticsearch_dir}/plugins/head" ]]; then
+          #{rubber_env.elasticsearch_dir}/bin/plugin --install mobz/elasticsearch-head;
         fi
       ENDSCRIPT
+
+      pip install elasticsearch-curator
     end
 
     after "rubber:bootstrap", "rubber:elasticsearch:bootstrap"
@@ -24,8 +32,6 @@ namespace :rubber do
     task :bootstrap, :roles => :elasticsearch do
       exists = capture("echo $(ls #{rubber_env.elasticsearch_data_dir} 2> /dev/null)")
       if exists.strip.size == 0
-        # After everything installed on machines, we need the source tree
-        # on hosts in order to run rubber:config for bootstrapping the db
         rubber.update_code_for_bootstrap
         rubber.run_config(:file => "role/elasticsearch/", :force => true, :deploy_path => release_path)
 
