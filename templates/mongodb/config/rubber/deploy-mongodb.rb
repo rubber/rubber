0c449f07b6d8f347e223ba3247c29ac4d41a40c9
Johnny Shields <johnny.shields@gmail.com>
2015-08-05 00:21:28 +0900

    More changes to support MongoDB 3.0

--- templates/mongodb/config/rubber/deploy-mongodb.rb
+++ templates/mongodb/config/rubber/deploy-mongodb.rb
@@ -1,20 +1,20 @@
 
 namespace :rubber do
-  
+
   namespace :mongodb do
-    
+
     rubber.allow_optional_tasks(self)
-    
+
     before "rubber:install_packages", "rubber:mongodb:setup_apt_sources"
-  
+
     task :setup_apt_sources, :roles => :mongodb do
       # Setup apt sources to mongodb from 10gen
       sources = <<-SOURCES
-        deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen
+        deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.0 multiverse
       SOURCES
       sources.gsub!(/^[ \t]*/, '')
-      put(sources, "/etc/apt/sources.list.d/mongodb.list") 
-      rsudo "apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10"
+      put(sources, "/etc/apt/sources.list.d/mongodb-org-3.0.list")
+      rsudo "apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10"
     end
 
     after "rubber:bootstrap", "rubber:mongodb:bootstrap"
@@ -28,21 +28,21 @@ namespace :rubber do
         restart
       end
     end
-    
+
     desc <<-DESC
       Starts the mongodb daemon
     DESC
     task :start, :roles => :mongodb do
-      rsudo "service mongodb start"
+      rsudo "service mongod status || service mongod start"
     end
-    
+
     desc <<-DESC
       Stops the mongodb daemon
     DESC
     task :stop, :roles => :mongodb do
-      rsudo "service mongodb stop || true"
+      rsudo "service mongod stop || true"
     end
-    
+
     desc <<-DESC
       Restarts the mongodb daemon
     DESC
