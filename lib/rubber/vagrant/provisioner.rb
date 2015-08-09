6dea8d50ceef53c16f15caaa342f933b2e2f8cce
Kevin Menard <kevin@nirvdrum.com>
2015-08-08 23:04:32 -0400

    Better environment cleanup in the Vagrant provisioner.

--- lib/rubber/vagrant/provisioner.rb
+++ lib/rubber/vagrant/provisioner.rb
@@ -40,71 +40,86 @@ module VagrantPlugins
       def create
         if config.use_vagrant_ruby
           script = "RUN_FROM_VAGRANT=true RUBBER_ENV=#{config.rubber_env} ALIAS=#{machine.name} ROLES='#{config.roles}' EXTERNAL_IP=#{private_ip} INTERNAL_IP=#{private_ip} RUBBER_SSH_KEY=#{rubber_ssh_keys} #{internal_cap_command} rubber:create -S initial_ssh_user=#{ssh_info[:username]}"
+          system(script)
         else
-          script = <<-ENDSCRIPT
-            #{clear_vagrant_environment}
+          Vagrant::Util::Env.with_clean_env do
+            script = <<-ENDSCRIPT
+              #{clear_vagrant_environment}
 
-            RUN_FROM_VAGRANT=true RUBBER_ENV=#{config.rubber_env} ALIAS=#{machine.name} ROLES='#{config.roles}' EXTERNAL_IP=#{private_ip} INTERNAL_IP=#{private_ip} RUBBER_SSH_KEY=#{rubber_ssh_keys} bash -c '#{rvm_prefix} bundle exec cap rubber:create -S initial_ssh_user=#{ssh_info[:username]}'
-          ENDSCRIPT
+              RUN_FROM_VAGRANT=true RUBBER_ENV=#{config.rubber_env} ALIAS=#{machine.name} ROLES='#{config.roles}' EXTERNAL_IP=#{private_ip} INTERNAL_IP=#{private_ip} RUBBER_SSH_KEY=#{rubber_ssh_keys} bash -c '#{rvm_prefix} bundle exec cap rubber:create -S initial_ssh_user=#{ssh_info[:username]}'
+            ENDSCRIPT
+
+            system(script)
+          end
         end
-
-        system(script)
       end
 
       def destroy
         if config.use_vagrant_ruby
           script = "RUN_FROM_VAGRANT=true FORCE=true RUBBER_ENV=#{config.rubber_env} ALIAS=#{machine.name} #{internal_cap_command} rubber:destroy"
+          system(script)
         else
-          script = <<-ENDSCRIPT
-            #{clear_vagrant_environment}
+          Vagrant::Util::Env.with_clean_env do
+            script = <<-ENDSCRIPT
+              #{clear_vagrant_environment}
 
-            RUN_FROM_VAGRANT=true FORCE=true RUBBER_ENV=#{config.rubber_env} ALIAS=#{machine.name} bash -c '#{rvm_prefix} bundle exec cap rubber:destroy'
-          ENDSCRIPT
+              RUN_FROM_VAGRANT=true FORCE=true RUBBER_ENV=#{config.rubber_env} ALIAS=#{machine.name} bash -c '#{rvm_prefix} bundle exec cap rubber:destroy'
+            ENDSCRIPT
+
+            system(script)
+          end
         end
-
-        system(script)
       end
 
       def refresh
         if config.use_vagrant_ruby
           script = "RUN_FROM_VAGRANT=true RUBBER_ENV=#{config.rubber_env} RUBBER_SSH_KEY=#{rubber_ssh_keys} ALIAS=#{machine.name} EXTERNAL_IP=#{private_ip} INTERNAL_IP=#{private_ip} #{internal_cap_command} rubber:refresh -S initial_ssh_user=#{ssh_info[:username]}"
+          system(script)
         else
-          script = <<-ENDSCRIPT
-            #{clear_vagrant_environment}
+          Vagrant::Util::Env.with_clean_env do
+            script = <<-ENDSCRIPT
+              #{clear_vagrant_environment}
 
-            RUN_FROM_VAGRANT=true RUBBER_ENV=#{config.rubber_env} RUBBER_SSH_KEY=#{rubber_ssh_keys} ALIAS=#{machine.name} EXTERNAL_IP=#{private_ip} INTERNAL_IP=#{private_ip} bash -c '#{rvm_prefix} bundle exec cap rubber:refresh -S initial_ssh_user=#{ssh_info[:username]}'
-          ENDSCRIPT
+              RUN_FROM_VAGRANT=true RUBBER_ENV=#{config.rubber_env} RUBBER_SSH_KEY=#{rubber_ssh_keys} ALIAS=#{machine.name} EXTERNAL_IP=#{private_ip} INTERNAL_IP=#{private_ip} bash -c '#{rvm_prefix} bundle exec cap rubber:refresh -S initial_ssh_user=#{ssh_info[:username]}'
+            ENDSCRIPT
+
+            system(script)
+          end
         end
-
-        system(script)
       end
 
       def bootstrap
         if config.use_vagrant_ruby
           script = "RUN_FROM_VAGRANT=true RUBBER_ENV=#{config.rubber_env} RUBBER_SSH_KEY=#{rubber_ssh_keys} FILTER=#{machine.name} #{internal_cap_command} rubber:bootstrap"
+          system(script)
         else
-          script = <<-ENDSCRIPT
-            #{clear_vagrant_environment}
+          Vagrant::Util::Env.with_clean_env do
+            script = <<-ENDSCRIPT
+              #{clear_vagrant_environment}
 
-            RUN_FROM_VAGRANT=true RUBBER_ENV=#{config.rubber_env} RUBBER_SSH_KEY=#{rubber_ssh_keys} FILTER=#{machine.name} bash -c '#{rvm_prefix} bundle exec cap rubber:bootstrap'
-          ENDSCRIPT
+              RUN_FROM_VAGRANT=true RUBBER_ENV=#{config.rubber_env} RUBBER_SSH_KEY=#{rubber_ssh_keys} FILTER=#{machine.name} bash -c '#{rvm_prefix} bundle exec cap rubber:bootstrap'
+            ENDSCRIPT
+
+            system(script)
+          end
         end
-
-        system(script)
       end
 
       def deploy_migrations
         if config.use_vagrant_ruby
           script = "RUN_FROM_VAGRANT=true RUBBER_ENV=#{config.rubber_env} RUBBER_SSH_KEY=#{rubber_ssh_keys} FILTER=#{machine.name} #{internal_cap_command} deploy:migrations"
+          system(script)
         else
-          script = <<-ENDSCRIPT
-          #{clear_vagrant_environment}
+          Vagrant::Util::Env.with_clean_env do
+            script = <<-ENDSCRIPT
+              #{clear_vagrant_environment}
 
-          RUN_FROM_VAGRANT=true RUBBER_ENV=#{config.rubber_env} RUBBER_SSH_KEY=#{rubber_ssh_keys} FILTER=#{machine.name} bash -c '#{rvm_prefix} bundle exec cap deploy:migrations'
-          ENDSCRIPT
+              RUN_FROM_VAGRANT=true RUBBER_ENV=#{config.rubber_env} RUBBER_SSH_KEY=#{rubber_ssh_keys} FILTER=#{machine.name} bash -c '#{rvm_prefix} bundle exec cap deploy:migrations'
+            ENDSCRIPT
+
+            system(script)
+          end
         end
-
-        system(script)
       end
 
       def internal_cap_command
@@ -117,16 +132,7 @@ module VagrantPlugins
 
       def clear_vagrant_environment
         <<-ENDSCRIPT
-          unset GEM_HOME;
           unset GEM_PATH;
-          unset RUBYOPT;
-          unset GEMRC;
-          unset BUNDLE_GEMFILE;
-          unset BUNDLE_BIN_PATH;
-          unset RUBYLIB;
-          unset BUNDLE_APP_CONFIG;
-
-          export PATH=#{ENV['PATH'].split(':').reject { |entry| entry =~ /vagrant/i }.join(':')};
         ENDSCRIPT
       end
     end
