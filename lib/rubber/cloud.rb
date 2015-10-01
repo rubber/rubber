89d37e1a270d902f2a01a72af848bb4ae30a946e
Dave Benvenuti <davebenvenuti@gmail.com>
2015-10-01 17:40:17 -0400

    load aws_vpc provider if aws config has vpc options

--- lib/rubber/cloud.rb
+++ lib/rubber/cloud.rb
@@ -5,8 +5,15 @@ module Rubber
 
     def self.get_provider(provider, env, capistrano)
       require "rubber/cloud/#{provider}"
-      clazz = Rubber::Cloud.const_get(Rubber::Util.camelcase(provider))
       provider_env = env.cloud_providers[provider]
+
+      if provider_env.vpc
+        require "rubber/cloud/aws_vpc"
+        clazz = Rubber::Cloud::AwsVpc
+      else
+        clazz = Rubber::Cloud.const_get(Rubber::Util.camelcase(provider))
+      end
+
       return clazz.new(provider_env, capistrano)
     end
 
