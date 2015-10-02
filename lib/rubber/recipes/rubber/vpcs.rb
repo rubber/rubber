fe475ef4450c1e695bc69b1ff1b6eb00ce17600b
Dave Benvenuti <davebenvenuti@gmail.com>
2015-10-02 14:25:43 -0400

    added a destroy_vpc task, whitespace fixes

--- lib/rubber/recipes/rubber/vpcs.rb
+++ lib/rubber/recipes/rubber/vpcs.rb
@@ -4,18 +4,33 @@ namespace :rubber do
   DESC
 
   required_task :setup_vpc do
-    vpc_id = cloud.setup_vpc
+    cloud.setup_vpc
+  end
 
-    if vpc_id
-      rubber_instances.artifact['vpc'] = vpc_id
-      rubber_instances.save
+  required_task :destroy_vpc do
+    env = rubber_cfg.environment.bind(nil, [])
+    vpc_cfg = env.rubber_instances.artifacts['vpc']
+
+    if vpc_cfg.length == 0
+      fatal("No VPC configured", 0)
     end
+
+    value = Capistrano::CLI.ui.ask("About to DESTROY #{vpc_cfg['id']} in mode #{Rubber.env}.  Are you SURE [yes/NO]?: ")
+
+    fatal("Exiting", 0) if value != "yes"
+
+    cloud.destroy_subnet(vpc_cfg['public_subnet'])
+    cloud.destroy_subnet(vpc_cfg['private_subnet'])
+    cloud.destroy_vpc(vpc_cfg['id'])
+
+    env.rubber_instances.artifacts['vpc'] = {}
+    env.rubber_instances.save
   end
 
   def get_vpc
     env = rubber_cfg.environment.bind(nil, [])
-    
-    env.rubber_instances.artifacts['vpc']   
+
+    env.rubber_instances.artifacts['vpc']
   end
 end
 
