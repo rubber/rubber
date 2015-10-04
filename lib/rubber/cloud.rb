10dec9c9b3e4dd099cff95c5b1a65b9422696be2
Dave Benvenuti <davebenvenuti@gmail.com>
2015-10-04 18:04:13 -0400

    we don't need a reference to the exception thrown

--- lib/rubber/cloud.rb
+++ lib/rubber/cloud.rb
@@ -12,7 +12,7 @@ module Rubber
       begin
         factory = Rubber::Cloud.const_get(Rubber::Util.camelcase(provider))::Factory
         return factory.get_provider(provider_env, capistrano)
-      rescue NameError => e
+      rescue NameError
         clazz = Rubber::Cloud.const_get(Rubber::Util.camelcase(provider))
         return clazz.new(provider_env, capistrano)
       end
