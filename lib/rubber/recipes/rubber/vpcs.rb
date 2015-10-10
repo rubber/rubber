3691fd90ffa1fb66f8eb845dfd89e062c71493ee
Dave Benvenuti <davebenvenuti@gmail.com>
2015-10-10 12:21:02 -0400

    added some descriptions

--- lib/rubber/recipes/rubber/vpcs.rb
+++ lib/rubber/recipes/rubber/vpcs.rb
@@ -4,9 +4,8 @@ class PrivateNic < Struct.new(:subnet_cidr, :gateway); end
 
 namespace :rubber do
   desc <<-DESC
-    Sets up the environment-wide VPC
+    Sets up the VPC identified by vpc_alias
   DESC
-
   required_task :create_vpc do
     vpc_alias = get_env("ALIAS", "VPC Alias: ", true, Rubber.config.cloud_providers['aws'].vpc_alias)
     vpc_cidr = get_env("CIDR", "CIDR Block (eg: 10.0.0.0/16): ", true, Rubber.config.cloud_providers['aws'].vpc_cidr)
@@ -14,6 +13,9 @@ namespace :rubber do
     cloud.setup_vpc(vpc_alias, vpc_cidr)
   end
 
+  desc <<-DESC
+    Configure a new subnet on the configured VPC
+  DESC
   required_task :create_vpc_subnet do
     vpc_alias = get_env("ALIAS", "VPC Alias: ", true, Rubber.config.cloud_providers['aws'].vpc_alias)
     cidr = get_env("CIDR", "CIDR Block (eg: 10.0.0.0/24): ", true)
@@ -37,6 +39,9 @@ namespace :rubber do
     cloud.setup_vpc_subnet(vpc_id, vpc_alias, private_nic, zone, name)
   end
 
+  desc <<-DESC
+    Destroy any VPC with a Rubber-defined Alias
+  DESC
   required_task :destroy_vpc do
     vpc_alias = get_env("ALIAS", "VPC Alias: ", true, Rubber.config.cloud_providers['aws'].vpc_alias)
     value = Capistrano::CLI.ui.ask("Are you sure you want to destroy vpc #{vpc_alias} [yes/NO]?: ")
@@ -48,6 +53,10 @@ namespace :rubber do
     end
   end
 
+  desc <<-DESC
+    Refresh the public gateway on a public subnet, or configure a new NAT instance on a private subnet.
+    This currently does not support switching a public subnet to private or vice versa.
+  DESC
   required_task :update_vpc_gateway do
     vpc_alias = get_env("ALIAS", "VPC Alias: ", true, Rubber.config.cloud_providers['aws'].vpc_alias)
     zone = get_env("ZONE", "Availability Zone: ", true, Rubber.config.cloud_providers['aws'].availability_zone)
